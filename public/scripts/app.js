91af4949a04e3aa28a7d62c9f376ac3361b2f2cf
ahza64 <ahza64@yahoo.com>
2016-04-06 17:36:10 -0700

    displaying form and title

---
+++ public/scripts/app.js
@@ -0,0 +1,13 @@
+console.log("JS is definitely working");
+
+$(document).ready(function() {
+  console.log('app.js loaded!');
+
+$('#newProjectForm').on('click' function(){
+  console.log('guuuud');
+});
+
+
+
+
+});//end of doc.ready
