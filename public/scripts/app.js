7a2adc4ec3668994d378fba9fb3d612fd3e31a17
ahza64 <ahza64@yahoo.com>
2016-04-06 18:43:41 -0700

    serialized data on console log with proper name data keys

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,9 +3,10 @@ console.log("JS is definitely working");
 $(document).ready(function() {
   console.log('app.js loaded!');
 
-$('#newProjectForm').on('click', function(e){
+$('#newProjectForm').on('submit', function(e){
   e.preventDefault();
   console.log('guuuud');
+  console.log('serialized info from Project form ', $(this).serialize());
 });
 
 
