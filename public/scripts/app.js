e32ab61ed47386313f64697fcc7f9639b292841a
ahza64 <ahza64@yahoo.com>
2016-04-06 17:39:06 -0700

    submit button console logging wihtout page refresh

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,7 +3,8 @@ console.log("JS is definitely working");
 $(document).ready(function() {
   console.log('app.js loaded!');
 
-$('#newProjectForm').on('click' function(){
+$('#newProjectForm').on('click', function(e){
+  e.preventDefault();
   console.log('guuuud');
 });
 
